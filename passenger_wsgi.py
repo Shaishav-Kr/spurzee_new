41b92af6ba6a04507b7675afcc7d4788d70075d3
Shaishav <84002444+Shaishav-Kr@users.noreply.github.com>
2024-07-31 19:55:21 +0530

    fyers changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -61,104 +61,15 @@ users = {'admin': {'password': 'user98440', 'role': 'admin'},
          'user1': {'password': 'user@123', 'role': 'admin'}
          }
 
+with open("abcd.txt", 'r') as r:
+    access_token = r.read()
 
-# redirect_uri= "http://127.0.0.1"  ## redircet_uri you entered while creating APP.
-# client_id = "DZO41L3M36-100"                       ## Client_id here refers to APP_ID of the created app
-# secret_key = "FDGH8EAMHW"                          ## app_secret key which you got after creating the app
-# grant_type = "authorization_code"                  ## The grant_type always has to be "authorization_code"
-# response_type = "code"                             ## The response_type always has to be "code"
-# state = "sample"                                   ##  The state field here acts as a session manager. you will be sent with the state field after successfull generation of auth_code
-# FY_ID="YL00137"
-# TOTP_KEY="EWQ3JH35FTQA6IQLYNZEGNB7WACXTRSG"
-# PIN="8844"
+client_id = "DZO41L3M36-100" # Lokesh
+# client_id = "7DRCQEYSRQ-100" # Trupthi
 
-# ### Connect to the sessionModel object here with the required input parameters
-# appSession = fyersModel.SessionModel(client_id = client_id, redirect_uri = redirect_uri,response_type=response_type,state=state,secret_key=secret_key,grant_type=grant_type)
-
-# # ## Make  a request to generate_authcode object this will return a login url which you need to open in your browser from where you can get the generated auth_code
-# generateTokenUrl = appSession.generate_authcode()
-# generateTokenUrl
-# pd.set_option('display.max_columns', None)
-# warnings.filterwarnings('ignore')
-
-# import base64
-# def getEncodedString(string):
-#     string = str(string)
-#     base64_bytes = base64.b64encode(string.encode("ascii"))
-#     return base64_bytes.decode("ascii")
-
-
-# URL_SEND_LOGIN_OTP="https://api-t2.fyers.in/vagator/v2/send_login_otp_v2"
-# res = requests.post(url=URL_SEND_LOGIN_OTP, json={
-#                     "fy_id": getEncodedString(FY_ID), "app_id": "2"}).json()
-# print(res)
-
-# if datetime.now().second % 30 > 27 : sleep(5)
-# URL_VERIFY_OTP="https://api-t2.fyers.in/vagator/v2/verify_otp"
-# res2 = requests.post(url=URL_VERIFY_OTP, json= {"request_key":res["request_key"],"otp":pyotp.TOTP(TOTP_KEY).now()}).json()
-# print(res2)
-
-
-# ses = requests.Session()
-# URL_VERIFY_OTP2="https://api-t2.fyers.in/vagator/v2/verify_pin_v2"
-# payload2 = {"request_key": res2["request_key"],"identity_type":"pin","identifier":getEncodedString(PIN)}
-# res3 = ses.post(url=URL_VERIFY_OTP2, json= payload2).json()
-# print(res3)
-
-
-# ses.headers.update({
-#     'authorization': f"Bearer {res3['data']['access_token']}"
-# })
-
-
-# TOKENURL="https://api-t1.fyers.in/api/v3/token"
-# payload3 = {"fyers_id":FY_ID,
-#           "app_id":client_id[:-4],
-#           "redirect_uri":redirect_uri,
-#           "appType":"100","code_challenge":"",
-#           "state":"None","scope":"","nonce":"","response_type":"code","create_cookie":True}
-
-# res3 = ses.post(url=TOKENURL, json= payload3).json()
-# print(res3)
-
-
-# url = res3['Url']
-# print(url)
-# parsed = urlparse(url)
-# auth_code = parse_qs(parsed.query)['auth_code'][0]
-# auth_code
-
-
-# grant_type = "authorization_code"
-
-# response_type = "code"
-
-# sess = fyersModel.SessionModel(
-#     client_id=client_id,
-#     secret_key=secret_key,
-#     redirect_uri=redirect_uri,
-#     response_type=response_type,
-#     grant_type=grant_type
-# )
-
-# # Set the authorization code in the session object
-# sess.set_token(auth_code)
-
-# # Generate the access token using the authorization code
-# response = sess.generate_token()
-
-# # Print the response, which should contain the access token and other details
-# #print(response)
-
-
-# access_token = response['access_token']
-
-# with open("abcd.txt", 'r') as r:
-#     access_token = r.read()
-access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MjIzMTI0MzAsImV4cCI6MTcyMjM4NTgxMCwibmJmIjoxNzIyMzEyNDMwLCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbXFHYnVqX3oxSVlGVGlFeHVfYTJVQkNFOU9yODdRMFJQOVZjTU8wRWpHR1pSNURNaWRFUXhwdkJMNEJoeUxwYmdIMVFqblFURkp4enE3MGp1UGk2SnFzdnBRZXA4TVA4WmxUS1BCQnN6Rzk1QWFhcz0iLCJkaXNwbGF5X25hbWUiOiJMT0tFU0ggVEFMTFVSSSIsIm9tcyI6IksxIiwiaHNtX2tleSI6IjgzZmZjNDBhNDBhNmMzMmVhODEyZmZlNjg4MDg2ZjA2NGE2NTU4OGU5NTEyNjdhOTA4MDQzMjU3IiwiZnlfaWQiOiJZTDAwMTM3IiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.qFC2Rb1tv-zGlO_b9uvWDuJApZ_sQkAKbFlEVrY9ZAM'
-client_id = "DZO41L3M36-100"
 fyers = fyersModel.FyersModel(
     client_id=client_id, is_async=False, token=access_token, log_path=os.getcwd())
+fyers.get_profile()
 
 
 def read_stocks_from_file():
@@ -1413,7 +1324,6 @@ def fetch_latest_data(symbol):
 
 
 dtime_datetime = dt.datetime.now()
-dtime_datetime += timedelta(hours=12, minutes=30)
 dtime = dtime_datetime.strftime("%Y-%m-%d")
 
 
